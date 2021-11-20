22698784a93c4f6df14118119b70ae6c818496ac
Andrey Poplavskii <andris838@gmail.com>
2021-11-20 22:52:16 +0300

    added class Downloader

--- Events/Events/ImageDownloader.cs
+++ Events/Events/ImageDownloader.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Events
 {
-    class ImageDownloader
+    public class ImageDownloader
     {
+
+        public void Download(string remoteUri, string fileName)
+        { 
+           
+            var _webclient = new WebClient();
+
+            Console.WriteLine("Качаю \"{0}\" из \"{1}\" .......\n\n", fileName, remoteUri);
+
+            _webclient.DownloadFile(remoteUri, fileName);
+
+            Console.WriteLine("Успешно скачал \"{0}\" из \"{1}\"", fileName, remoteUri);
+
+        }
+
     }
 }
--- Events/Events/Program.cs
+++ Events/Events/Program.cs
@@ -6,7 +6,18 @@ namespace Events
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string remoteUri = "https://effigis.com/wp-content/uploads/2015/02/Iunctus_SPOT5_5m_8bit_RGB_DRA_torngat_mountains_national_park_8bits_1.jpg";
+
+            string fileName = "bigimage.jpg";
+
+            ImageDownloader myimageDownloader = new ImageDownloader();
+
+            myimageDownloader.Download(remoteUri, fileName);
+
+            Console.WriteLine("Для выхода из программы нажмите любую клавишу");
+            Console.ReadKey();
+
         }
     }
 }
